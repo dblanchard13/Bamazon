115f35a6342d7084cbba36b6a443143c1ea84d7a
Andy Scott <andy@aoscott.com>
2017-04-22 12:25:34 -0700

    initial commit

---
+++ bamazonCustomer.js
@@ -0,0 +1,58 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "P@$$w0rd",
+  database: "Bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+});
+
+var runSearch = function() {
+  inquirer.prompt({
+    name: "action",
+    type: "list",
+    message: "What would you like to do?",
+    choices: ["Find songs by artist", "Find all artists who appear more than once",
+      "Find data within a specific range", "Search for a specific song"]
+  }).then(function(answer) {
+
+    switch (answer.action) {
+      case "Find songs by artist":
+        artistSearch();
+        break;
+
+      case "Find all artists who appear more than once":
+        multiSearch();
+        break;
+
+      case "Find data within a specific range":
+        rangeSearch();
+        break;
+
+      case "Search for a specific song":
+        songSearch();
+        break;
+    }
+  });
+};
+
+var showProducts = function() {
+  var query = "SELECT ";
+  connection.query(query, function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].artist);
+    }
+    runSearch();
+  });
+};
